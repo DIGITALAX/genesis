4e5ca8dff0d895a4a9c076bb9cea05cca47b8cf6
emmajane1313 <emma.jane@digitalax.xyz>
2025-11-19 01:48:01 -0500

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {},
+  serverExternalPackages: ["pino", "pino-pretty", "thread-stream"],
+  reactStrictMode: false,
+  experimental: {
+    scrollRestoration: true,
+    optimizeCss: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false,
+      };
+    }
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "thread-stream/test": false,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
